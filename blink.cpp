f43cdab8c9a4a38021a25f8cd4f4ede8cd85984b
Youngwook Go <113055584+Youngwook-Go@users.noreply.github.com>
2023-09-22 03:56:58 +0000

    add blink cpp file

---
+++ blink.cpp
@@ -0,0 +1,20 @@
+/*
+Created by: Youngwook Go
+Created on: Sep 2023
+Blinks LED for increasing time
+*/
+
+int blinkTime = 1000;
+
+void setup() {
+  pinMode(LED_BUILTIN,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(blinkTime);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+
+  blinkTime = blinkTime + 1000;
+}
